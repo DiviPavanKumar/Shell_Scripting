b6e812790227d44e5204ca1f1cd96d9eb11bd5f1
Pavan Kumar Divi <pavandivi.kumar47@gmail.com>
2025-03-20 17:02:58 +0530

    VariablePracticing

--- Variable.sh
+++ Variable.sh
@@ -11,8 +11,6 @@ CITY="Hyderabad"
 A="10"
 B="20"
 
-sum="$(( NUM1 + NUM2 ))"
-
 # Printing variables
 
 echo "My name is $NAME"
@@ -25,5 +23,11 @@ echo "After Swap: A=$B, B=$A"
 read -p "Enter First number: " NUM1
 read -p "Enter 2nd number: " NUM2
 
+sum=$((NUM1 + NUM2))
+diff=$((NUM1 - NUM2))
+Mul=$((NUM1 * NUM2))
+
 echo "Addition: $sum"
+echo "Subtraction: $diff"
+echo "Multiplication: $Mul"
 
