c9df2771618f8e1f0e3a401e23afee01cf257619
Pavan Kumar Divi <pavandivi.kumar47@gmail.com>
2025-03-20 16:13:04 +0530

    VariablePracticing

---
+++ Variable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Hello World! practicing Variables samples"
+
+# Defining Variables
+
+NAME="Pavan"
+AGE="32"
+CITY="Hyderabad"
+
+# Printing variables
+
+echo "My name is $NAME"
+echo "Im $AGE years old"
+echo "Im staying in $CITY"
