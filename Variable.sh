ba823af6090c2fcd801530336b4dd19072ee8d54
Pavan Kumar Divi <pavandivi.kumar47@gmail.com>
2025-03-20 16:41:30 +0530

    VariablePracticing

--- Variable.sh
+++ Variable.sh
@@ -11,6 +11,9 @@ CITY="Hyderabad"
 A="10"
 B="20"
 
+NUMBER1=$1
+NUMBER2=$2
+
 # Printing variables
 
 echo "My name is $NAME"
@@ -20,4 +23,7 @@ echo "Im staying in $CITY"
 echo "Before Swap: A=$A, B=$B"
 echo "After Swap: A=$B, B=$A"
 
+echo "Enter First number: $1"
+echo "Enter 2nd number: $2"
+echo "Addition= $(NUMBER1+NUMBER2)"
 
