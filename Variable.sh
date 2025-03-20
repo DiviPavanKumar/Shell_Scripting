67a41fd357ad12b3af911fd7bd94a42828a109ac
Pavan Kumar Divi <pavandivi.kumar47@gmail.com>
2025-03-20 16:23:58 +0530

    VariablePracticing

--- Variable.sh
+++ Variable.sh
@@ -8,8 +8,15 @@ NAME="Pavan"
 AGE="32"
 CITY="Hyderabad"
 
+A="10"
+B="20"
+
 # Printing variables
 
 echo "My name is $NAME"
 echo "Im $AGE years old"
 echo "Im staying in $CITY"
+
+echo "A=$B"
+echo "B=$A"
+
